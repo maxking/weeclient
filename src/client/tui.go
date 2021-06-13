ee9d47f64c4c1e3d96bcfa8c0add6240be394431
Abhilash Raj <raj.abhilash1@gmail.com>
2021-06-12 20:40:29 -0700

    Add support for updating the current buffer with any lines added.
    This is a significant update which adds support to receive updates
    from weechat and show in the current buffer.

--- src/client/tui.go
+++ src/client/tui.go
@@ -2,6 +2,7 @@ package client
 
 import (
 	"fmt"
+	"strings"
 
 	"github.com/maxking/weeclient/src/weechat"
 	"github.com/rivo/tview"
@@ -38,18 +39,20 @@ type TerminalView struct {
 	grid *tview.Grid
 
 	bufferList *BufferListWidget
-	buffer     *tview.TextView
+	pages      *tview.Pages
+	buffers    map[string]*tview.TextView
 }
 
 // Event handlers.
 func (tv *TerminalView) SetCurrentBuffer(index int, mainText, secondaryText string, shortcut rune) {
 	buf := tv.bufferList.getByFullName(mainText)
 	if buf != nil {
-		tv.buffer.SetText(fmt.Sprintf("index: %v primary: %v secondary: %v\n----\n %v\n%v",
-			index, mainText, secondaryText, buf.FullName, buf.Title))
-	} else {
-		tv.buffer.SetText(fmt.Sprintf("Buffer not found! \nindex: %v primary: %v secondary: %v\n%v\n=====\n",
-			index, mainText, secondaryText, tv.bufferList))
+		// For the buffer widget, set the right number of lines.
+		if bufView, ok := tv.buffers[buf.FullName]; ok {
+			bufView.SetText((fmt.Sprintf("[%v] %v\n-----\n%v", buf.FullName, buf.Title, strings.Join(buf.Lines, "\n"))))
+		}
+		// Then, switch to the page that is embedding the above buffer widget.
+		tv.pages.SwitchToPage(fmt.Sprintf("page-%v", mainText))
 	}
 }
 
@@ -58,6 +61,24 @@ func (tv *TerminalView) HandleListBuffers(buflist map[string]*weechat.WeechatBuf
 	for ptr, buf := range buflist {
 		tv.bufferList.List.AddItem(buf.FullName, "", 'u', nil)
 		tv.bufferList.Buffers[ptr] = buf
+		bufferView := tview.NewTextView().
+			SetTextAlign(tview.AlignLeft).
+			SetWordWrap(true).
+			SetDynamicColors(true).
+			SetText(fmt.Sprintf("[%v] %v\n-----\n%v", buf.FullName, buf.Title, strings.Join(buf.Lines, "\n")))
+
+		bufferView.SetTitle(buf.FullName)
+		bufferView.SetChangedFunc(func() { tv.app.Draw() })
+
+		tv.pages.AddPage(fmt.Sprintf("page-%v", buf.FullName),
+			bufferView,
+			true,
+			// Only the core.weechat buffer is visible at first.
+			buf.FullName == "core.weechat",
+		)
+
+		tv.buffers[buf.FullName] = bufferView
+
 	}
 }
 
@@ -70,7 +91,12 @@ func (tv *TerminalView) HandleNickList() {
 }
 
 func (tv *TerminalView) HandleLineAdded(buffer, message string) {
-
+	buf := tv.bufferList.Buffers[buffer]
+	buf.Lines = append(buf.Lines, message)
+	// Also, add the message to the current view.
+	if bufView, ok := tv.buffers[buf.FullName]; ok {
+		bufView.Write([]byte("\n" + message))
+	}
 }
 
 func (tv *TerminalView) Default(msg *weechat.WeechatMessage) {
@@ -80,20 +106,18 @@ func (tv *TerminalView) Default(msg *weechat.WeechatMessage) {
 func TviewStart(weechan chan *weechat.WeechatMessage) {
 	app := tview.NewApplication()
 
-	textView := tview.NewTextView().
-		SetTextAlign(tview.AlignCenter).
-		SetText("Sample")
-
 	bufffers := make(map[string]*weechat.WeechatBuffer)
 	buflist := NewBufferListWidget(bufffers)
+	bufferspage := tview.NewPages()
+	bufferViews := make(map[string]*tview.TextView, 100)
 
 	grid := tview.NewGrid().
 		SetColumns(-1, -4).
 		SetBorders(true).
 		AddItem(buflist.List, 0, 0, 1, 1, 0, 0, true).
-		AddItem(textView, 0, 1, 1, 1, 0, 0, false)
+		AddItem(bufferspage, 0, 1, 1, 1, 0, 0, false)
 
-	view := &TerminalView{app: app, grid: grid, bufferList: buflist, buffer: textView}
+	view := &TerminalView{app: app, grid: grid, bufferList: buflist, pages: bufferspage, buffers: bufferViews}
 	view.bufferList.List.SetChangedFunc(view.SetCurrentBuffer)
 
 	// Read from the weechat incoming queue and enquee for handling.
